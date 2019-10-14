e1221f5ecc7f8776686b00fa1ae9d0a974c2dbba
Benjamin-DCI <benjamin.harris@digitalcareerinstitute.org>
2019-10-14 20:52:16 +0200

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,34 @@
+export const SELECT_CHANNEL = "SELECT_CHANNEL";
+export const REQUEST_POSTS = "REQUEST_POSTS";
+export const RECEIVE_POSTS = "RECEIVE_POSTS";
+
+const MY_API_KEY = "6ff22ce13e854e228643151ba46d6cb1";
+
+export const getChannel = channel => ({
+	type: SELECT_CHANNEL,
+	channel
+});
+
+export const requestPosts = () => ({
+	type: REQUEST_POSTS
+});
+
+export const receivedPosts = json => ({
+	type: RECEIVE_POSTS,
+	json: json.articles
+});
+
+export function fetchPosts(channel) {
+	return function(dispatch) {
+		dispatch(requestPosts());
+		return fetch(`https://newsapi.org/v1/articles?
+        source=${channel}&apiKey=${MY_API_KEY}`)
+			.then(
+				response => response.json(),
+				error => console.log("An error occured", error)
+			)
+			.then(json => {
+				dispatch(receivedPosts(json));
+			});
+	};
+}
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ChannelField from "./ChannelField";
+import Button from "../containers/Button";
+import TopNews from "../containers/TopNews";
+
+const App = () => (
+	<div>
+		<ChannelField />
+		<Button />
+		<TopNews />
+	</div>
+);
+
+export default App;
